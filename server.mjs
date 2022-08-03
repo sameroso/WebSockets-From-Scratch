e0e6f3e0e6ad11b7e55d6f5197091e7c7c4ccb55
Samer Kayali <samerkkr@gmail.com>
2022-08-02 22:22:55 -0300

    feat: create error handling

--- server.mjs
+++ server.mjs
@@ -8,3 +8,12 @@ createServer((req, res) => {
 }).listen(PORT, () => {
   console.log("sertver listening to ", PORT);
 });
+
+// error handling to keep server on
+["uncaughtException", "unhandledRejection"].forEach((ev) =>
+  process.on(ev, (err) => {
+    console.error(
+      `something bad happened! event: ${ev}, msg: ${err.stack || err}`
+    );
+  })
+);
