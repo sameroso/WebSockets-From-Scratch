0339410a3ad9ab208d7bca38bef4d57d4b545803
Samer Kayali <samerkkr@gmail.com>
2022-08-02 22:34:59 -0300

    added listener to socket on server

--- server.mjs
+++ server.mjs
@@ -2,11 +2,15 @@ import { createServer } from "http";
 
 const PORT = 1337;
 
-createServer((req, res) => {
+const server = createServer((req, res) => {
   res.writeHead(200);
   res.end("hey there");
 }).listen(PORT, () => {
   console.log("sertver listening to ", PORT);
+});
+
+server.on("upgrade", (req, socket, headers) => {
+  console.log({ req, socket, headers });
 });
 
 // error handling to keep server on
